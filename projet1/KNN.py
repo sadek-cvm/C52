45be646f118d8a845ba9fe6cfb266ed748a7afa3
Sadek Ahmed <e.asadek@etu.cvm.qc.ca>
2023-10-27 14:21:25 -0400

    Fd

--- projet1/KNN.py
+++ projet1/KNN.py
@@ -21,6 +21,8 @@ class KNN:
             raise TypeError("Dataset doit être de type ndarray")
         elif value.ndim != 2:
             raise ValueError("Dataset doit être une liste à 2 dimensions")
+        elif value.dtype != np.float64:
+            raise ValueError("Dataset doit contenir des valeurs de type float")
         elif value.shape[1] < 2: # minimun une coordonnée et le tag
             raise ValueError("Dataset doit avoir au moins deux elements (une coordonnée et le tag)")
         
@@ -37,6 +39,8 @@ class KNN:
             raise TypeError("Sample doit être de type ndarray")
         elif value.ndim != 1:
             raise ValueError("Sample doit être une liste à 1 dimension")
+        elif value.dtype != np.float64:
+            raise ValueError("Sample doit contenir des valeurs de type float")
         elif value.shape[0] != (self.__dataset[:, :-1]).shape[1]:
             raise ValueError("Les dimensions de sample et des coordonnées de dataset (sans compter les tags) doivent être égalaux")
 
@@ -69,50 +73,51 @@ class KNN:
         distances = self.__calculate_distances(sample)
         sorted_indices = np.argsort(distances) # ex: [2, 4, 5, 3, 1] (indices)
         nearest_indices = sorted_indices[:self.__k] # ex: [2, 4, 5] (indices)
-        nearest_neighbors = self.__dataset[nearest_indices] # ex: [[1, 2, 3, 0], [4, 5, 6, 0], [7, 8, 9, 1]] (coordonnées + tag)
+        nearest_neighbors = self.__dataset[nearest_indices] # ex: [[1.0 , 2.5, 3.1, 0.0], [4.2, 5.6, 6.7, 0.0], [7.1, 8.7, 9.2, 1.0]] (coordonnées + tag)
         return nearest_neighbors
     
-    def find_sample_tag(self, sample):
+    def find_sample_tag(self, sample) -> int:
         self.__validate_dataset(self.__dataset)
         self.__validate_sample(sample)
         nearest_neighbors = self.__find_nearest_neighbors(sample)
         nearest_tags = nearest_neighbors[:, -1]
+        nearest_tags = nearest_tags.astype(int) # on transform les tags in int (au lieu de float)
         tags_frequency = np.bincount(nearest_tags)
         sample_tag = np.argmax(tags_frequency) 
         # quand la méthode commence avec le mot "arg" cela veut dire qu'on prend la position de valeur et non pas la valeur
         return sample_tag
     
+    
 
 ##############################
-def test():
-    dataset1 = np.array([[2, 1, 2, 1],  #cercle
-                        [4, 5, 6, 2],   #carre
-                        [7, 3, 7, 3],   #triangle
-                        [5, 6, 6, 2],   #carre
-                        [6, 2, 8, 3],   #triangle
-                        [1, 2, 2, 1],   #cercle
-                        [4, 5, 5, 2],   #carre
-                        [7, 3, 8, 3],   #triangle
-                        [2, 1, 1, 1],   #cercle
-                        [7, 1, 5, 4]])  #rectangle
+def exemple():
     
-    dataset2 = np.array([[2, 1, 2, 1],  #cercle
-                        [4, 5, 6, 2],   #carre
-                        [7, 3, 7, 3],   #triangle
-                        [5, 6, 6, 2],   #carre
-                        [6, 2, 8, 3]])  #triangle
+    tags = ["Cercle", "Carré", "Rectangle"]
     
-    sample = np.array([7, 1, 6])
+    dataset = np.array([[2, 1, 2, 0],   #cercle
+                        [4, 5, 6, 1],   #carre
+                        [7, 3, 7, 2],   #triangle
+                        [5, 6, 6, 1],   #carre
+                        [6, 2, 8, 2],   #triangle
+                        [1, 2, 2, 0],   #cercle
+                        [4, 5, 5, 1],   #carre
+                        [7, 3, 8, 2],   #triangle
+                        [2, 1, 1, 0],   #cercle
+                        [3, 1, 2, 0]]   #rectangle
+                    ).astype(float)
+    
+    sample = np.array([8, 4, 7]).astype(float)
 
     knn = KNN()
-    knn.dataset = dataset1
-    knn.k = 3
-    sample_tag = knn.find_sample_tag(sample)
-    print(sample_tag)
+    knn.dataset = dataset
+    knn.k = 2
+    tag_index = knn.find_sample_tag(sample) # retourne un int
+    tag = tags[tag_index]
+    print(tag)
 
 if __name__ == "__main__":
-    test()
+    exemple()
 
 # trucs a faire:
 # une function qui donne le k maximal
-# verifier si le type des values et float
+# faire un singleton
